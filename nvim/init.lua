91226a0124cf34bd8feda051044c5eeaa29818a9
gaailiunas <gailiunaslaurynas@tutanota.com>
2025-07-25 16:21:58 +0300

    changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,13 +5,15 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 Plug('nvim-tree/nvim-web-devicons')
-Plug('tiagovla/tokyodark.nvim')
 Plug('nvim-tree/nvim-tree.lua')
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim', {['tag'] = '0.1.8'})
 Plug('neovim/nvim-lspconfig')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('hrsh7th/nvim-cmp')
+Plug('lukas-reineke/indent-blankline.nvim')
+Plug('nvim-lualine/lualine.nvim')
+Plug('EdenEast/nightfox.nvim')
 vim.call('plug#end')
 
 -- Treesitter configuration
@@ -23,8 +25,10 @@ require("nvim-treesitter.configs").setup{
 }
 
 -- Set colorscheme
-vim.cmd("colorscheme tokyodark")
+vim.cmd("colorscheme carbonfox")
 vim.cmd("syntax on")
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
 
 -- General settings
 vim.opt.tabstop = 4
@@ -32,22 +36,39 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.autoindent = true
-vim.opt.guicursor = "n-v-i-c:block-Cursor"
+vim.opt.guicursor = "i:block-blinkwait1000-blinkon500-blinkoff500";
 vim.opt.cursorline = true
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1f1f1f" })
+
+-- Indent Blankline setup
+require("ibl").setup()
+
+-- LuaLine setup
+require('lualine').setup()
+vim.opt.showmode = false
 
 -- Nvim Tree setup
 require("nvim-tree").setup()
 
 -- LSP configuration for clangd
+vim.diagnostic.config {
+    virtual_text = false,
+    float = {
+        header = false,
+        border = 'rounded',
+        focusable = false,
+    },
+}
+
 local lspconfig = require("lspconfig")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local cmp = require("cmp")  -- Require cmp here
 
 lspconfig.clangd.setup{
     capabilities = cmp_nvim_lsp.default_capabilities(),
+    handlers = handlers,
 }
 lspconfig.rust_analyzer.setup{
-
 }
 
 -- nvim-cmp setup
@@ -65,6 +86,10 @@ cmp.setup({
     experimental = {
         ghost_text = true,
     },
+    window =  {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    }
 })
 
 -- NvimTree
@@ -74,7 +99,16 @@ vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeFocus<cr>', { desc = 'Focus NvimT
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration', noremap = true, silent = true })
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition', noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fc', vim.lsp.buf.format, { desc = 'Format code', noremap = true, silent = true })
-vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, { desc = 'Signature help', noremap = true, silent = true })
+
+-- Custom signature help with border
+local function signature_help_with_border()
+    vim.lsp.buf.signature_help({
+        border = "rounded",
+        focusable = false,
+    })
+end
+
+vim.keymap.set({'i', 'v'}, '<C-s>', signature_help_with_border, { noremap = true, silent = true })
 
 -- Diagnostics
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostics', noremap = true, silent = true })
@@ -86,3 +120,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope: Find 
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope: Live grep', noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope: Buffers', noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope: Help tags', noremap = true, silent = true })
+
